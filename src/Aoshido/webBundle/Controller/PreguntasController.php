626608be46aba4fb2fa9e3ff9bcd46704ab95ad5
Aoshido <Aoshido@gmail.com>
2015-01-24 17:59:29 +0000

    Paginator Problem

--- src/Aoshido/webBundle/Controller/PreguntasController.php
+++ src/Aoshido/webBundle/Controller/PreguntasController.php
@@ -2,12 +2,45 @@
 
 namespace Aoshido\webBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class PreguntasController extends Controller
-{
-    public function newAction()
-    {
-        return $this->render('AoshidowebBundle:Preguntas:new.html.twig');
+class PreguntasController extends Controller {
+
+    public function newAction(Request $request) {
+        //Display a list of all Preguntas
+        $preguntas = $this->getDoctrine()
+                ->getRepository('AoshidowebBundle:Pregunta')
+                ->findBy(array('activo' => TRUE));
+
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate($preguntas, $this->getRequest()->query->get('page', 1), 10);
+        $pagination->setPageRange(6);
+
+        $cantidad = count($preguntas);
+        
+        $pregunta = new Pregunta();
+        $form = $this->createFormPregunta($pregunta);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $pregunta->setActivo(TRUE);
+            $pregunta->setVecesVista(0);
+            $pregunta->setVecesAcertada(0);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($pregunta);
+            $em->flush();
+            return $this->redirect($this->generateUrl('preguntas_ABM'));
+        }
+
+
+
+        return $this->render('AoshidowebBundle:Preguntas:new.html.twig', array(
+                    'form' => $form->createView(),
+                    'paginas' => $pagination,
+                    'cantidad' => $cantidad,
+        ));
     }
+
 }
---
+++ src/Aoshido/webBundle/form/PreguntaType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Aoshido\webBundle\form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+
+class PreguntaType extends AbstractType {
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder->add('contenido', 'text', array(
+            'label' => 'Pregunta:'
+        ));
+        
+        $builder->add('save', 'submit', array(
+            'label' => 'Agregar',
+        ));
+    }
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'Aoshido\studyBundle\Entity\Pregunta',
+        ));
+    }
+    public function getName() {
+        return 'pregunta';
+    }
+}
