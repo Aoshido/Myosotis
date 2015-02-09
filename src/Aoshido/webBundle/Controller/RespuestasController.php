5763f5ea2f39b2a8fdf57ce05a8d64f9da48d5e6
Aoshido <Aoshido@gmail.com>
2015-02-09 18:09:28 +0000

    Agregado agregar respuesta

---
+++ src/Aoshido/webBundle/Controller/RespuestasController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Aoshido\webBundle\Controller;
+
+use Aoshido\webBundle\Entity\Respuesta;
+use Aoshido\webBundle\Form\RespuestaType;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class RespuestasController extends Controller {
+
+    public function newAction(Request $request,$idPregunta) {
+        $respuestas = $this->getDoctrine()
+                ->getRepository('AoshidowebBundle:Respuesta')
+                ->findBy(array('activo' => TRUE , 'pregunta' => $idPregunta));
+
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate($respuestas, $this->getRequest()->query->get('page', 1), 10);
+        $pagination->setPageRange(6);
+
+        $cantidad = count($respuestas);
+
+        $respuesta = new Respuesta();
+        $form = $this->createForm(new RespuestaType(),$respuesta);
+
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $respuesta->setActivo(TRUE);
+            $pregunta = $this->getDoctrine()->getRepository('AoshidowebBundle:Pregunta')->find($idPregunta);
+            $respuesta->setPregunta($pregunta);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($respuesta);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('abms_preguntas'));
+        }
+
+        return $this->render('AoshidowebBundle:Respuestas:new.html.twig', array(
+                    'form' => $form->createView(),
+                    'paginas' => $pagination,
+                    'cantidad' => $cantidad,
+        ));
+    }
+
+    public function disableAction($idPregunta) {
+
+        $pregunta = $this->getDoctrine()
+                ->getRepository('AoshidowebBundle:Pregunta')
+                ->find($idPregunta);
+        
+        $pregunta->setActivo(false);
+        
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($pregunta);
+        $em->flush();
+        
+        return $this->redirect($this->generateUrl('abms_preguntas'));
+    }
+
+    public function MateriasByCarreraAction(Request $request) {
+        $idcarrera = $request->request->get('idcarrera');
+
+        $em = $this->getDoctrine()->getManager();
+        $qb = $em->getRepository("AoshidowebBundle:Materia")->createQueryBuilder('m')
+                ->where('m.activo=true')
+                ->innerJoin('m.carreras', 'c')
+                ->andWhere('c.activo=true')
+                ->andWhere('c=:idcarrera')
+                ->setParameter('idcarrera', $idcarrera)
+                ->addOrderBy('m.descripcion', 'ASC');
+
+        $materias = $qb->getQuery()->getArrayResult();
+
+        return new JsonResponse($materias);
+    }
+
+    public function TemasByMateriaAction(Request $request) {
+        $idmateria = $request->request->get('idmateria');
+
+        $em = $this->getDoctrine()->getManager();
+        $qb = $em->getRepository("AoshidowebBundle:Tema")->createQueryBuilder('t')
+                ->where('t.activo=true')
+                ->innerJoin('t.materia', 'm')
+                ->andWhere('m.activo=true')
+                ->andWhere('m=:idmateria')
+                ->setParameter('idmateria', $idmateria)
+                ->addOrderBy('m.descripcion', 'ASC');
+
+        $temas = $qb->getQuery()->getArrayResult();
+
+        return new JsonResponse($temas);
+    }
+
+}
---
+++ src/Aoshido/webBundle/form/RespuestaType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Aoshido\webBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class RespuestaType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder->add('contenido', 'text', array(
+            'label' => 'Contenido:',
+            'label_attr' => array(
+                'class' => 'col-md-2 control-label'
+            ),
+            'attr' => array(
+                'class' => 'form-control'
+            )
+        ));
+
+        $builder->add('correcta', 'choice', array(
+            'label' => 'Correcta:',
+            'choices'  => array('t' => 'Verdadero', 'f' => 'Falso'),
+            'required' => true,
+            'expanded' => true,
+            'multiple' => false,
+        ));
+
+        $builder->add('save', 'submit', array(
+            'label' => 'Agregar Respuesta',
+            'attr' => array(
+                'class' => 'btn btn-success'
+            ),
+        ));
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'Aoshido\webBundle\Entity\Respuesta',
+        ));
+    }
+
+    public function getName() {
+        return 'respuesta';
+    }
+
+}
