0065270d4dd4f82c765eeded043334a54fa62293
Aoshido <Aoshido@gmail.com>
2015-03-27 21:01:27 +0000

    restart

---
+++ src/Aoshido/webBundle/form/EventListener/AddTemaFieldSuscriber.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Aoshido\webBundle\Form\EventListener;
+
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Doctrine\ORM\EntityRepository;
+
+class AddTemaFieldSuscriber implements EventSubscriberInterface {
+
+    private $propertyPathToTema;
+
+    public function __construct($propertyPathToTema) {
+        $this->propertyPathToTema = $propertyPathToTema;
+    }
+
+    public static function getSubscribedEvents() {
+        return array(
+            FormEvents::PRE_SET_DATA => 'preSetData',
+            FormEvents::PRE_SUBMIT => 'preSubmit'
+        );
+    }
+
+    private function addTemaForm($form, $idmateria = null) {
+        $formOptions = array(
+            'class'         => 'AoshidowebBundle:Tema',
+            'empty_value'   => '- Seleccione Tema -',
+            'label'         => 'Tema:',
+            'mapped'        => false,
+            'attr'          => array(
+                'class' => 'tema_selector',
+            ),
+            'query_builder' => function (EntityRepository $repository) use ($idmateria) {
+                         $qb = $repository->createQueryBuilder('t')
+                        ->where('t.activo=true')
+                        ->innerJoin('t.materia', 'm')
+                        ->andWhere('m.activo=true')
+                        ->andWhere('m=:idmateria')
+                        ->setParameter('idmateria', $idmateria)
+                        ->addOrderBy('t.descripcion', 'ASC');
+                return $qb;
+            }
+        );
+
+        $form->add($this->propertyPathToTema, 'entity', $formOptions);
+    }
+
+    public function preSetData(FormEvent $event) {
+        $data = $event->getData();
+        $form = $event->getForm();
+
+        if (null === $data) {
+            return;
+        }
+
+        $accessor    = PropertyAccess::createPropertyAccessor();
+
+        $tema        = $accessor->getValue($data, $this->propertyPathToTema);
+        $materia_id = ($tema) ? $tema->getMateria()->getId() : null;
+ 
+        $this->addTemaForm($form, $materia_id);
+    }
+
+    public function preSubmit(FormEvent $event) {
+        $data = $event->getData();
+        $form = $event->getForm();
+
+        $idmateria = array_key_exists('idmateria', $data) ? $data['idmateria'] : null;
+        $this->addTemaForm($form, $idmateria);
+    }
+
+}
