198b8b0e1444d06ad5b21d9f9036f289c102f5ed
Aoshido <Aoshido@gmail.com>
2015-06-12 18:39:18 +0000

    Started Quiz functionality

--- src/Aoshido/webBundle/Controller/GamesController.php
+++ src/Aoshido/webBundle/Controller/GamesController.php
@@ -6,84 +6,42 @@ use Aoshido\webBundle\Entity\Pregunta;
 use Aoshido\webBundle\Form\PreguntaType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\Common\Collections\ArrayCollection;
 
-use Aoshido\webBundle\Form\Filter\PreguntaFilterType;
 
 class GamesController extends Controller {
 
     public function settingsAction(Request $request) {
 
-        
         $pregunta = new Pregunta();
-        $form = $this->createForm(new PreguntaType(), $pregunta , array('method' => 'PATCH'));
-        
+        $form = $this->createForm(new PreguntaType(), $pregunta);
+
         $form->handleRequest($request);
-        
-        $preguntas = $this->getDoctrine()
-                ->getRepository('AoshidowebBundle:Pregunta')
-                ->findBy(array('activo' => TRUE));
-        
+
         if ($form->isValid()) {
-            
-            if ($pregunta->getTemas() != ""){
-                
+            $preguntas = new ArrayCollection();
+            foreach ($pregunta->getTemas() as $tema) {
+                $preguntas = $tema->getPreguntas();
             }
-            
-            
-            return $this->redirect($this->generateUrl('abms_preguntas'));
+
+            return $this->quizAction($preguntas);
         }
-        
+
+        return $this->render('AoshidowebBundle:Games:quiz.html.twig', array(
+                    'form' => $form->createView(),
+        ));
+    }
+
+    public function quizAction($preguntas) {
         $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate($preguntas, $this->getRequest()->query->get('page', 1), 10);
         $pagination->setPageRange(6);
 
+        $cantidad = count($preguntas);
 
-        return $this->render('AoshidowebBundle:Games:quiz.html.twig', array(
-                    'form' => $form->createView(),
+        return $this->render('AoshidowebBundle:Games:play.html.twig', array(
                     'paginas' => $pagination,
+                    'cantidad' => $cantidad,
         ));
     }
-
-    public function disableAction($idPregunta) {
-
-        $this->get('service_disabler')->disablePregunta($idPregunta);
-
-        return $this->redirect($this->generateUrl('abms_preguntas'));
-    }
-
-    public function MateriasByCarreraAction(Request $request) {
-        $idcarrera = $request->request->get('idcarrera');
-
-        $em = $this->getDoctrine()->getManager();
-        $qb = $em->getRepository("AoshidowebBundle:Materia")->createQueryBuilder('m')
-                ->where('m.activo=true')
-                ->innerJoin('m.carreras', 'c')
-                ->andWhere('c.activo=true')
-                ->andWhere('c=:idcarrera')
-                ->setParameter('idcarrera', $idcarrera)
-                ->addOrderBy('m.descripcion', 'ASC');
-
-        $materias = $qb->getQuery()->getArrayResult();
-
-        return new JsonResponse($materias);
-    }
-
-    public function TemasByMateriaAction(Request $request) {
-        $idmateria = $request->request->get('idmateria');
-
-        $em = $this->getDoctrine()->getManager();
-        $qb = $em->getRepository("AoshidowebBundle:Tema")->createQueryBuilder('t')
-                ->where('t.activo=true')
-                ->innerJoin('t.materia', 'm')
-                ->andWhere('m.activo=true')
-                ->andWhere('m=:idmateria')
-                ->setParameter('idmateria', $idmateria)
-                ->addOrderBy('m.descripcion', 'ASC');
-
-        $temas = $qb->getQuery()->getArrayResult();
-
-        return new JsonResponse($temas);
-    }
-
 }
