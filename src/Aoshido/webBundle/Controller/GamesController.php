d76cadc913c17c89bb39d0e01821347ef3b35d1b
Aoshido <Aoshido@gmail.com>
2015-06-13 02:53:27 +0000

    Added cards action

--- src/Aoshido/webBundle/Controller/GamesController.php
+++ src/Aoshido/webBundle/Controller/GamesController.php
@@ -22,34 +22,43 @@ class GamesController extends Controller {
     }
 
     public function quizAction(Request $request) {
-        
+
         $pregunta = new Pregunta();
         $form = $this->createForm(new PreguntaType(), $pregunta);
 
         $form->handleRequest($request);
-        
+
         $preguntas = new ArrayCollection();
-        if($form->isValid()){
+        if ($form->isValid()) {
             foreach ($pregunta->getTemas() as $tema) {
                 $preguntas_temp = $tema->getPreguntas();
                 foreach ($preguntas_temp as $pregunta_temp) {
-                    if ($pregunta_temp != $pregunta && !$preguntas->contains($pregunta_temp)){
+                    if ($pregunta_temp != $pregunta && !$preguntas->contains($pregunta_temp)) {
                         $preguntas->add($pregunta_temp);
                     }
                 }
             }
         }
-        
+
         $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate($preguntas, $this->getRequest()->query->get('page', 1), 10);
         $pagination->setPageRange(6);
 
         $cantidad = count($preguntas);
         
-        return $this->render('AoshidowebBundle:Games:play.html.twig', array(
-                    'paginas' => $pagination,
-                    'cantidad' => $cantidad,
-        ));
+        switch ($request->getPathInfo()) {
+            case '/games/quiz':
+                return $this->render('AoshidowebBundle:Games:quiz.html.twig', array(
+                            'paginas' => $pagination,
+                            'cantidad' => $cantidad,
+                ));
+            case '/games/cards':
+                return $this->render('AoshidowebBundle:Games:cards.html.twig', array(
+                            'paginas' => $pagination,
+                            'cantidad' => $cantidad,
+                ));
+        }
+        return $this->redirect($this->generateUrl('abms_preguntas'));
     }
 
 }
