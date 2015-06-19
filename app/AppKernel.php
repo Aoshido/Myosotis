5770885b6aedba85781d550904dda9da20d69135
Aoshido <Aoshido@gmail.com>
2015-06-19 17:25:15 +0000

    dear god the trip

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Aoshido\userBundle\AoshidouserBundle(),
             new Lexik\Bundle\FormFilterBundle\LexikFormFilterBundle(),
+            new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
