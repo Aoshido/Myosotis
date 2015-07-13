f75f3ce12c84066e70859a215a77036068bfce0a
Aoshido <Aoshido@gmail.com>
2015-07-13 15:24:30 +0000

    Fixing Cards styles

---
+++ refresh_cache.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo php app/console cache:clear --env=dev
+sudo php app/console cache:clear --env=prod
+
+sudo php app/console assets:install web
+
+sudo php app/console assetic:dump --env=prod
+sudo php app/console assetic:dump --env=dev
+
+sudo chmod -R 777 /var/www/aosymf/.*
+
