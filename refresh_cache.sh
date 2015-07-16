4f7831e2712993424a0891605d468e8bcc01fba3
Maximiliano Tejada <mtejada@cajadepagos.com>
2015-07-16 20:10:12 +0000

    separando div vermas del div de contenido

--- refresh_cache.sh
+++ refresh_cache.sh
@@ -8,5 +8,5 @@ sudo php app/console assets:install web
 sudo php app/console assetic:dump --env=prod
 sudo php app/console assetic:dump --env=dev
 
-sudo chmod -R 777 /var/www/aosymf/.*
+sudo chmod -R 777 /var/server/www/aoshido/.*
 
