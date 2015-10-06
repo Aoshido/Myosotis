27f1a3aeb514fbe945b9eba87e3bec039d6d582e
Aoshido <Aoshido@gmail.com>
2015-10-06 01:06:04 +0000

    added send mail feature

--- src/Aoshido/webBundle/Controller/DefaultController.php
+++ src/Aoshido/webBundle/Controller/DefaultController.php
@@ -5,17 +5,27 @@ namespace Aoshido\webBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
-    public function indexAction(Request $request)
-    {
+class DefaultController extends Controller {
+
+    public function indexAction(Request $request) {
         $locale = $request->getLocale();
-        return $this->render('AoshidowebBundle:Default:index.html.twig',array(
-            'locale' => $locale));
+        return $this->render('AoshidowebBundle:Default:index.html.twig', array(
+                    'locale' => $locale));
     }
 
-    public function bioAction()
-    {
+    public function bioAction() {
+        $message = \Swift_Message::newInstance()
+                ->setSubject('Hola, soy tama del Futuro!')
+                ->setFrom('notifications@aoshido.com.ar')
+                ->setTo('aoshido@gmail.com')
+                ->setBody(
+                $this->renderView(
+                        'Emails/welcome.html.twig', array('name' => 'Aca va el placeholder')
+                ), 'text/html'
+                )
+        ;
+        $this->get('mailer')->send($message);
         return $this->render('AoshidowebBundle:Default:bio.html.twig');
     }
+
 }
