134c688000ae944a78f48391f75ac4f1ed70d0ea
Aoshido <Aoshido@gmail.com>
2015-10-12 20:16:28 +0000

    Rerouted CARDS game

--- src/Aoshido/webBundle/Controller/GamesController.php
+++ src/Aoshido/webBundle/Controller/GamesController.php
@@ -10,6 +10,42 @@ use Doctrine\Common\Collections\ArrayCollection;
 
 class GamesController extends Controller {
 
+    public function cardsAction(Request $request) {
+
+        $pregunta = new Pregunta();
+
+        $form = $this->createForm(new PreguntaType(), $pregunta, array(
+            'method' => 'GET',
+        ));
+
+        $form->handleRequest($request);
+
+        $preguntas = new ArrayCollection();
+
+        if ($form->isValid()) {
+            foreach ($pregunta->getTemas() as $tema) {
+                $preguntas_temp = $tema->getPreguntas();
+                foreach ($preguntas_temp as $pregunta_temp) {
+                    if ($pregunta_temp != $pregunta && !$preguntas->contains($pregunta_temp)) {
+                        $preguntas->add($pregunta_temp);
+                    }
+                }
+            }
+        }
+
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate($preguntas, $this->getRequest()->query->get('page', 1), 4);
+        $pagination->setPageRange(6);
+
+        $cantidad = count($preguntas);
+
+        return $this->render('AoshidowebBundle:Games:cards.html.twig', array(
+                    'form' => $form->createView(),
+                    'paginas' => $pagination,
+                    'cantidad' => $cantidad,
+        ));
+    }
+
     public function quizAction(Request $request) {
 
         $pregunta = new Pregunta();
