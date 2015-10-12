67347dec8e7b7e541f777bf1710480ea2fe3b26c
Aoshido <Aoshido@gmail.com>
2015-10-12 22:03:13 +0000

    Working entity

---
+++ src/Aoshido/webBundle/Form/ExamenType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Aoshido\webBundle\Form;
+
+use Aoshido\webBundle\Form\TemaType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Aoshido\webBundle\Form\EventListener\AddTemaFieldSuscriber;
+use Aoshido\webBundle\Form\EventListener\AddMateriaFieldSuscriber;
+use Aoshido\webBundle\Form\EventListener\AddCarreraFieldSuscriber;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
+
+class ExamenType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder->add('preguntas', 'collection', array(
+            'type' => new PreguntaType(),
+            'mapped' => true,
+            'allow_add' => true,
+            'allow_delete' => true,
+            'by_reference' => false,
+        ));
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'Aoshido\webBundle\Entity\Examen',
+        ));
+    }
+
+    public function getName() {
+        return 'quiz';
+    }
+
+}
