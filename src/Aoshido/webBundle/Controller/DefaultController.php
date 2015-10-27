f32ef1715c8e4b3f2eb84f166843ba76a9887fe1
Aoshido <Aoshido@gmail.com>
2015-10-27 15:08:42 +0000

    New registered action

--- src/Aoshido/webBundle/Controller/DefaultController.php
+++ src/Aoshido/webBundle/Controller/DefaultController.php
@@ -10,6 +10,11 @@ class DefaultController extends Controller {
     public function indexAction(Request $request) {
         $locale = $request->getLocale();
 
+        $this->get('session')->getFlashBag()->add('warning', 'Warning, ojo e' );
+        $this->get('session')->getFlashBag()->add('success', 'Dale que vaa');
+        $this->get('session')->getFlashBag()->add('danger', 'Danger, algo se cago');
+        $this->get('session')->getFlashBag()->add('info', 'EEste de info es horrible');
+        
         return $this->render('AoshidowebBundle:Default:index.html.twig', array(
                     'locale' => $locale));
     }
