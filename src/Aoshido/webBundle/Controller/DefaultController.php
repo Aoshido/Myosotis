92b650a51549aa5dcfb2a91bc4917a4ae268174e
Aoshido <Aoshido@gmail.com>
2015-10-27 14:57:45 +0000

    New registered action

--- src/Aoshido/webBundle/Controller/DefaultController.php
+++ src/Aoshido/webBundle/Controller/DefaultController.php
@@ -9,14 +9,15 @@ class DefaultController extends Controller {
 
     public function indexAction(Request $request) {
         $locale = $request->getLocale();
+
         return $this->render('AoshidowebBundle:Default:index.html.twig', array(
                     'locale' => $locale));
     }
-
+    
     public function bioAction() {
         return $this->render('AoshidowebBundle:Default:bio.html.twig');
     }
-    
+
     public function faqAction() {
         return $this->render('AoshidowebBundle:Default:faq.html.twig');
     }
