f4a4bda265e705efb58d935879ecdc5ce13ee7e6
Aoshido <Aoshido@gmail.com>
2016-04-27 04:10:34 +0000

    Fugly looking admin, but at least shows some good data

--- src/Aoshido/adminBundle/Controller/DashboardController.php
+++ src/Aoshido/adminBundle/Controller/DashboardController.php
@@ -10,4 +10,58 @@ class DashboardController extends Controller {
         return $this->render('AoshidoadminBundle:Dashboard:index.html.twig');
     }
 
+    public function lastQuestionsAction() {
+        $filterBuilder = $this->get('doctrine.orm.entity_manager')
+                ->getRepository('AoshidowebBundle:Pregunta')
+                ->createQueryBuilder('p')
+                //->where('p.activo = TRUE')
+                ->orderBy('p.creada','DESC');
+
+        $preguntas = $filterBuilder->getQuery()->getResult();
+
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate($preguntas, $this->getRequest()->query->get('page', 1), 10);
+        $pagination->setPageRange(6);
+
+        return $this->render('AoshidoadminBundle:Dashboard:_lastQuestionsBlock.html.twig', array(
+                    'paginas' => $pagination,
+        ));
+    }
+    
+    public function lastUsersAction() {
+        $filterBuilder = $this->get('doctrine.orm.entity_manager')
+                ->getRepository('AoshidoUserBundle:User')
+                ->createQueryBuilder('u')
+                //->where('p.activo = TRUE')
+                ->orderBy('u.id','DESC');
+
+        $preguntas = $filterBuilder->getQuery()->getResult();
+
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate($preguntas, $this->getRequest()->query->get('page', 1), 10);
+        $pagination->setPageRange(6);
+
+        return $this->render('AoshidoadminBundle:Dashboard:_lastUsersBlock.html.twig', array(
+                    'paginas' => $pagination,
+        ));
+    }
+    
+    public function lastBugsAction() {
+        $filterBuilder = $this->get('doctrine.orm.entity_manager')
+                ->getRepository('AoshidowebBundle:Bug')
+                ->createQueryBuilder('b')
+                //->where('p.activo = TRUE')
+                ->orderBy('b.id','DESC');
+
+        $preguntas = $filterBuilder->getQuery()->getResult();
+
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate($preguntas, $this->getRequest()->query->get('page', 1), 10);
+        $pagination->setPageRange(6);
+
+        return $this->render('AoshidoadminBundle:Dashboard:_lastBugsBlock.html.twig', array(
+                    'paginas' => $pagination,
+        ));
+    }
+
 }
