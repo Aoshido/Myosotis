0a8f11553b312fca101db629761e7a8c6591be56
Aoshido <Aoshido@gmail.com>
2016-05-13 18:32:06 +0000

    Better dashboard
        - Dashboard now brings data from the controller
        - Added new "blocks" and modified the routing of the dashboard
            - Users block
            - Bugs block

--- src/Aoshido/adminBundle/Controller/DashboardController.php
+++ src/Aoshido/adminBundle/Controller/DashboardController.php
@@ -3,15 +3,41 @@
 namespace Aoshido\adminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DashboardController extends Controller {
 
     public function indexAction() {
-        return $this->render('AoshidoadminBundle:Dashboard:index.html.twig');
+        $filterBuilder = $this->get('doctrine.orm.entity_manager')
+                ->getRepository('AoshidowebBundle:Materia')
+                ->createQueryBuilder('m')
+                ->where('m.activo = TRUE');
+
+        $materias = $filterBuilder->getQuery()->getResult();
+
+        foreach ($materias as $materia) {
+            $preguntas = 0;
+            foreach ($materia->getTemas() as $tema) {
+                $preguntas += sizeof($tema->getPreguntas());
+            }
+            $preguntasPorMateria[$materia->getDescripcion()] = $preguntas;
+        }
+
+        return $this->render('AoshidoadminBundle:Dashboard:index.html.twig', array('preguntasPorMateria' => $preguntasPorMateria));
     }
 
     public function getMaxMemoryAction() {
-        return $this->render('AoshidoadminBundle:Dashboard:test.html.twig', array('memory' => memory_get_usage()));
+        $i = 0;
+        while ($i < 100) {
+            //$memoryArray[] = (memory_get_usage() / 1024);
+            $memoryArray[] = rand(0, 100);
+            $i++;
+        }
+        // create a JSON-response with a 200 status code
+        $response = new Response(json_encode($memoryArray),Response::HTTP_OK);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
     }
 
     public function lastQuestionsAction() {
@@ -32,7 +58,7 @@ class DashboardController extends Controller {
         ));
     }
 
-    public function lastUsersAction() {
+    public function UsersAction() {
         $filterBuilder = $this->get('doctrine.orm.entity_manager')
                 ->getRepository('AoshidoUserBundle:User')
                 ->createQueryBuilder('u')
@@ -45,12 +71,12 @@ class DashboardController extends Controller {
         $pagination = $paginator->paginate($preguntas, $this->getRequest()->query->get('page', 1), 10);
         $pagination->setPageRange(6);
 
-        return $this->render('AoshidoadminBundle:Dashboard:_lastUsersBlock.html.twig', array(
+        return $this->render('AoshidoadminBundle:Dashboard:Users.html.twig', array(
                     'paginas' => $pagination,
         ));
     }
 
-    public function lastBugsAction() {
+    public function BugsAction() {
         $filterBuilder = $this->get('doctrine.orm.entity_manager')
                 ->getRepository('AoshidowebBundle:Bug')
                 ->createQueryBuilder('b')
@@ -63,7 +89,7 @@ class DashboardController extends Controller {
         $pagination = $paginator->paginate($preguntas, $this->getRequest()->query->get('page', 1), 10);
         $pagination->setPageRange(6);
 
-        return $this->render('AoshidoadminBundle:Dashboard:_lastBugsBlock.html.twig', array(
+        return $this->render('AoshidoadminBundle:Dashboard:Bugs.html.twig', array(
                     'paginas' => $pagination,
         ));
     }
