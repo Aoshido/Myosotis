14873c1804e875e49aeb3bdc40d9e51d34b5ed34
Aoshido <Aoshido@gmail.com>
2016-07-12 05:51:02 +0000

    Add Experience, Refactor Challenge, Add Test
    	- Add experience to users. Now users can gain experience by answering questions.
    	Experience is handled by a listener on questions "veces acertada"
    	- Challenge underwent a refactor. The method "IsAnswseredWith($answer) was added
    	to the questions.
    	- Try to add test for IsAnsweredWith. Can get to work ORM

--- src/Aoshido/UserBundle/Entity/User.php
+++ src/Aoshido/UserBundle/Entity/User.php
@@ -4,6 +4,7 @@ namespace Aoshido\UserBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use FOS\UserBundle\Model\User as BaseUser;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
@@ -17,12 +18,52 @@ class User extends BaseUser {
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
-    
-    
+
+    /**
+     * @ORM\Column(name="current_experience", type="integer")
+     */
+    protected $currentExperience;
+
     public function __construct() {
         parent::__construct();
         // your own logic
         $this->addRole('ROLE_STUDENT');
+    }
+
+    /**
+     * Set currentExperience
+     *
+     * @param integer $currentExperience
+     *
+     * @return User
+     */
+    public function setCurrentExperience($currentExperience) {
+        $this->currentExperience = $currentExperience;
+
+        return $this;
+    }
+
+    /**
+     * Get currentExperience
+     *
+     * @return integer
+     */
+    public function getCurrentExperience() {
+        return $this->currentExperience;
+    }
+    
+    
+    /**
+     * Set currentExperience
+     *
+     * @param integer $experience
+     *
+     * @return User
+     */
+    public function addExperience($experience) {
+        $this->currentExperience += $experience;
+
+        return $this;
     }
 
 }
