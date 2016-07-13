5373d23ae9d12ccc276a3dfec1c369709cdc46d8
Aoshido <Aoshido@gmail.com>
2016-07-13 04:38:23 +0000

    Add Level, Broke UserAdmin
    	- Now users have levels, they follow an exponential formula
    	- UserAdmin stopped working
    	- Fixed a bug on challenge mode that made questions go to
    	different enviroments

--- src/Aoshido/webBundle/Admin/UserAdmin.php
+++ src/Aoshido/webBundle/Admin/UserAdmin.php
@@ -10,22 +10,10 @@ use Sonata\AdminBundle\Form\FormMapper;
 class UserAdmin extends AbstractAdmin {
 
     protected function configureFormFields(FormMapper $formMapper) {
-        $container = $this->getConfigurationPool()->getContainer();
-        $roles = $container->getParameter('security.role_hierarchy.roles');
-
-        $rolesChoices = self::flattenRoles($roles);
-
         $formMapper->add('username', 'text');
         $formMapper->add('email', 'text');
         $formMapper->add('roles', 'text');
         $formMapper->add('enabled', 'text');
-        $formMapper
-                //...
-                ->add('roles', 'choice', array(
-                    'choices' => $rolesChoices,
-                    'multiple' => true
-                        )
-        );
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper) {
@@ -47,31 +35,12 @@ class UserAdmin extends AbstractAdmin {
         $listMapper->add('enabled');
         $listMapper->add('locked');
     }
-
+    
     public function toString($object) {
-        return $object instanceof User ? $object->getUsername() : 'User'; // shown in the breadcrumb on the create view
+        return $object instanceof User 
+                ? $object->getUsername() 
+                : 'User'; // shown in the breadcrumb on the create view
     }
 
-    /**
-     * Turns the role's array keys into string <ROLES_NAME> keys.
-     * @todo Move to convenience or make it recursive ? ;-)
-     */
-    protected static function flattenRoles($rolesHierarchy) {
-        $flatRoles = array();
-        foreach ($rolesHierarchy as $roles) {
-
-            if (empty($roles)) {
-                continue;
-            }
-
-            foreach ($roles as $role) {
-                if (!isset($flatRoles[$role])) {
-                    $flatRoles[$role] = $role;
-                }
-            }
-        }
-
-        return $flatRoles;
-    }
 
 }
